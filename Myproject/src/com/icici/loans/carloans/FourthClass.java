41abb3b8570a4b978a4208889ddd3659b326eb8a
Saikande6 <saikande6@gmail.com>
2018-07-11 16:34:29 +0530

    Added one more class

--- Myproject/src/com/icici/loans/carloans/FourthClass.java
+++ Myproject/src/com/icici/loans/carloans/FourthClass.java
@@ -1,12 +1,10 @@
 package com.icici.loans.carloans;
 
-public class ThirdClass {
+public class FourthClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("checking");
-		System.out.println("chexk");
+
 	}
-	
 
 }
