da3aaabddb9f17fc44e82fed1185d7ac8091b0b3
Saikande6 <saikande6@gmail.com>
2018-07-17 16:34:50 +0530

    sample

--- Myproject/src/com/icici/loans/carloans/ThirdClass.java
+++ Myproject/src/com/icici/loans/carloans/ThirdClass.java
@@ -5,7 +5,7 @@ public class ThirdClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("checking");
-		System.out.println("chexk");
+		  System.out.println("chexk");
 		System.out.println("check1");
 		
 	}
